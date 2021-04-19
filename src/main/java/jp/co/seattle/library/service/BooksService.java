7793c7341c381799dc1492aa0c5a98e1cfac9f21
hoshi ayane <ayane@hoshinoMacBook-Air.local>
2021-04-19 15:10:31 +0900

    タスク２完了

--- src/main/java/jp/co/seattle/library/service/BooksService.java
+++ src/main/java/jp/co/seattle/library/service/BooksService.java
@@ -33,7 +33,7 @@ public class BooksService {
 
         // TODO 取得したい情報を取得するようにSQLを修正
         List<BookInfo> getedBookList = jdbcTemplate.query(
-                "select * from books",
+                "select title,author,publisher,publish_data from books order title by asc",
                 new BookInfoRowMapper());
 
         return getedBookList;

dc95477fc27a4f884e62cb448ca32a2869a26aa9
hoshi ayane <ayane@hoshinoMacBook-Air.local>
2021-05-10 09:08:49 +0900

    タスク８修正

--- src/main/java/jp/co/seattle/library/controller/BulkController.java
+++ src/main/java/jp/co/seattle/library/controller/BulkController.java
@@ -25,7 +25,7 @@ import jp.co.seattle.library.dto.BookDetailsInfo;
 import jp.co.seattle.library.service.BooksService;
 
 /**
- * .
+ * 一括登録コントローラー
  */
 @Controller //APIの入り口
 public class BulkController {
@@ -41,15 +41,8 @@ public class BulkController {
     }
 
     /**
-     * 書籍情報を登録する
-     * @param locale ロケール情報
-     * @param title 書籍名
-     * @param description 説明文
-     * @param author 著者名
-     * @param publisher 出版社
-     * @param publish_date 出版日
-     * @param isbn ISBN 
-     * @param file サムネイルファイル
+     * 書籍情報を一括登録する 
+     * @param file CSVファイル
      * @param model モデル
      * @return 遷移先画面
      */
@@ -69,7 +62,7 @@ public class BulkController {
             BufferedReader buf = new BufferedReader(reader);
             String errorMessage = "";//箱を作る
             int rowCount= 1; //csvファイルの行の番号（何行目）
-            boolean flag = false;//変数に初期値としてfalseを入れる
+            boolean errorFlag = false;//変数に初期値としてfalseを入れる
 
             // ファイルを行単位で読む
             //while文では読んで値を保持していく作業が行われている。
@@ -82,9 +75,12 @@ public class BulkController {
                 //isEmptyは文字の長さが０の状態
                 if (br[0].isEmpty() || br[1].isEmpty() || br[2].isEmpty() || br[3].isEmpty()) {
                     errorMessage += rowCount + "行目で必要な情報がありません"; //空だった時にエラーの処理する
-                    flag=true;
+                    errorFlag = true;
 
                 }
+                //ファイルを閉じる
+                buf.close();
+
                 //出版日のバリデーションチェック
                 if(br[3] != null);{
                     try {
@@ -93,19 +89,19 @@ public class BulkController {
                         d1.parse(br[3]);
                     } catch (Exception e) {
                         errorMessage += rowCount + "行目の出版日は半角数字のYYYYMMDD形式で入力してください"; //空だった時にエラーの処理する
-                        flag = true;
+                        errorFlag = true;
 
                     }
                 }
-                    //ISBNのバリデーションチェック
-                    if (br[4] != null && !(br[4].isEmpty())
-                            && !(br[4].matches("([0-9]{10}|[0-9]{13})?")))
-                        ;
-                    {
-                        errorMessage += rowCount + "行目のISBNの桁数または半角数字が正しくありません"; //空だった時にエラーの処理する
-                        flag = true;//trueの時にif文は実行される。エラーが起きたことを示すためにtrueを代入する
+                //ISBNのバリデーションチェック
+                if (!(br[4].isEmpty())
+                        && !(br[4].matches("([0-9]{10}|[0-9]{13})?")))
+                    ;
+                {
+                    errorMessage += rowCount + "行目のISBNの桁数または半角数字が正しくありません"; //空だった時にエラーの処理する
+                    errorFlag = true;//trueの時にif文は実行される。エラーが起きたことを示すためにtrueを代入する
 
-                    }
+                }
 
                 // 書籍情報をDtoに格納する。
                 BookDetailsInfo bookInfo = new BookDetailsInfo();
@@ -119,7 +115,7 @@ public class BulkController {
                 bookcsv.add(bookInfo);
             }
             //エラーがあった場合の処理
-            if (flag) {
+            if (errorFlag) {
                 model.addAttribute("error", errorMessage);
                 return "bulkBook";
             }
@@ -128,24 +124,23 @@ public class BulkController {
             for (BookDetailsInfo book : bookcsv) {
                 booksService.registBook(book);
             }
-                model.addAttribute("resultMessage", "登録完了");
+            model.addAttribute("resultMessage", "登録完了");
 
-                model.addAttribute("bookDetailsInfo", booksService.getBookInfo(booksService.getBookId()));
-                //  詳細画面に遷移する
-                return "bulkBook";
+            model.addAttribute("bookDetailsInfo", booksService.getBookInfo(booksService.getBookId()));
+            //  一括登録画面に遷移する
+            return "bulkBook";
 
 
+        } catch (IOException e1) {
+            model.addAttribute("errorMessage", "CSVファイル読み込みでエラーが発生しました。");
+            return "bulkBook";
 
-    } catch (IOException e1) {
-        model.addAttribute("errorMessage", "CSVファイル読み込みでエラーが発生しました。");
-        return "bulkBook";
+        } catch (Exception e2) {
+            model.addAttribute("errorMessage", "CSVファイル読み込みでエラーが発生しました。");
+            return "bulkBook";
 
-    } catch (Exception e2) {
-            model.addAttribute("errorMessage","CSVファイル読み込みでエラーが発生しました。" );
-        return "bulkBook";
 
+        }
 
     }
-
-}
 }
